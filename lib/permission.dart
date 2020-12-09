97fdf30b541d53a3b79c1d5047730100a1ba8778
Olderrm <Olderrm@augsburg.edu>
2020-12-08 22:02:33 -1000

    Added more permissions to the setting page

--- lib/permission.dart
+++ lib/permission.dart
@@ -33,6 +33,32 @@ class permission extends State<Perpart> {
     return ward;
   }
 
+
+  Future<bool> requestGalleryPermission({Function onPressedDenied}) async {
+    var ward = await requestPermission(PermissionGroup.photos);
+    if (!ward) {
+      onPressedDenied();
+    }
+    return ward;
+  }
+
+  Future<bool> requestMicPermission({Function onPressedDenied}) async {
+    var ward = await requestPermission(PermissionGroup.microphone);
+    if (!ward) {
+      onPressedDenied();
+    }
+    return ward;
+  }
+
+  Future<bool> requestStorePermission({Function onPressedDenied}) async {
+    var ward = await requestPermission(PermissionGroup.storage);
+    if (!ward) {
+      onPressedDenied();
+    }
+    return ward;
+  }
+
+
   Future<bool> hasPermission(PermissionGroup permission) async {
     var permissionStatus =
     await phandler.checkPermissionStatus(permission);
@@ -42,21 +68,65 @@ class permission extends State<Perpart> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Notescout',
-        home: Scaffold(
-            body: Center(
+        return Scaffold(
+            body:  Stack(children: <Widget>[(
+            Align(
+                alignment: Alignment.centerLeft,
               child: MaterialButton(
                 color: Colors.blue,
-                child: Text('Request Permissione to use the camera'),
+                child: Text('Camera Permission'),
                 onPressed: () {
                   permission().requestCameraPermission(
                       onPressedDenied: () {
                         print('Permission hs been denied');
                       });
-                },
-              ),
+                }
+              )
             )
-        ));
+        ),
+
+        Align(
+        alignment: Alignment.topCenter,
+          child: MaterialButton(
+            color: Colors.blue,
+            child: Text('Gallery Permission'),
+            onPressed: () {
+              permission().requestGalleryPermission(
+                  onPressedDenied: () {
+                    print('Permission hs been denied');
+                  });
+            }
+          )
+        ),
+    Align(
+    alignment: Alignment.bottomCenter,
+    child: MaterialButton(
+    color: Colors.blue,
+    child: Text('Microphone Permission'),
+    onPressed: () {
+    permission().requestMicPermission(
+    onPressedDenied: () {
+    print('Permission hs been denied');
+    });
+    }
+    )
+    ),
+    Align(
+    alignment: Alignment.centerRight,
+    child: MaterialButton(
+    color: Colors.blue,
+    child: Text('Storage Permission'),
+    onPressed: () {
+    permission().requestStorePermission(
+    onPressedDenied: () {
+    print('Permission hs been denied');
+    });
+    }
+    )
+    ),
+    ]
+    )
+        );
+
   }
 }
